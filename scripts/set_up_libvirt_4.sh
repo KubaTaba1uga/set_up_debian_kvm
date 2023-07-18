d73dc5ebfa5e94ef2e68dfb46e15186458f12985
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-07-18 20:04:40 +0200

    Update set_up_libvirt_4.sh

--- scripts/set_up_libvirt_4.sh
+++ scripts/set_up_libvirt_4.sh
@@ -33,4 +33,4 @@ echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
 echo "net.ipv4.conf.all.forwarding=1" | sudo tee -a /etc/sysctl.conf
 sudo sysctl -p
 
-sudo iptables-restore .nat-iptables
+sudo iptables-restore scripts/.nat-iptables
