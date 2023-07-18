ba50d4fdb9eb033786a94e67fa99f6cbe6567b74
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-07-18 20:02:40 +0200

    Update set_up_libvirt_4.sh

--- scripts/set_up_libvirt_4.sh
+++ scripts/set_up_libvirt_4.sh
@@ -12,9 +12,9 @@ sudo virsh net-destroy default
 sudo virsh net-autostart --disable default
 
 RANDOM_MAC=$(hexdump -vn3 -e '/3 "52:54:00"' -e '/1 ":%02x"' -e '"\n"' /dev/urandom)
-ip link add $BRIDGE_NAME-dummy address $RANDOM_MAC type dummy
+sudo ip link add $BRIDGE_NAME-dummy address $RANDOM_MAC type dummy
 
-auto $BRIDGE_NAME
+echo "auto $BRIDGE_NAME
 iface $BRIDGE_NAME inet static
     # Make sure bridge-utils is installed!
     bridge_ports $BRIDGE_NAME-dummy
@@ -22,3 +22,15 @@ iface $BRIDGE_NAME inet static
     bridge_fd 2
     address $BRIDGE_IP
     netmask 255.255.255.0
+    " | sudo tee -a /etc/network/interfaces
+
+sudo apt-get install -y net-tools
+
+sudo ifup $BRIDGE_NAME-dummy
+sudo ifup $BRIDGE_NAME
+
+echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
+echo "net.ipv4.conf.all.forwarding=1" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
+
+sudo iptables-restore .nat-iptables
