e27eb666e0458534abfbbea7225bec7551c6cd54
Taba1uga <73971628+KubaTaba1uga@users.noreply.github.com>
2023-07-18 19:55:32 +0200

    Update set_up_libvirt_4.sh

--- scripts/set_up_libvirt_4.sh
+++ scripts/set_up_libvirt_4.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+BRIDGE_NAME=virt-nat-bridge 
+BRIDGE_IP=10.0.0.1
+
 sudo systemctl enable --now libvirtd 
 
 sudo usermod -a -G libvirt $USER 
 
-sudo virsh net-start default
-sudo virsh net-autostart default
+# Custom Nat network
+sudo virsh net-destroy default
+sudo virsh net-autostart --disable default
+
+RANDOM_MAC=$(hexdump -vn3 -e '/3 "52:54:00"' -e '/1 ":%02x"' -e '"\n"' /dev/urandom)
+ip link add $BRIDGE_NAME-dummy address $RANDOM_MAC type dummy
+
+auto $BRIDGE_NAME
+iface $BRIDGE_NAME inet static
+    # Make sure bridge-utils is installed!
+    bridge_ports $BRIDGE_NAME-dummy
+    bridge_stp on
+    bridge_fd 2
+    address $BRIDGE_IP
+    netmask 255.255.255.0
